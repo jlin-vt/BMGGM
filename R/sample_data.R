3a4ad970d51e2fc7f19cf007be36c2e98495daf4
jlin-vt <jiali@vt.edu>
2019-11-28 13:05:53 -0800

    add travis.yml

--- R/sample_data.R
+++ R/sample_data.R
@@ -1,33 +1,33 @@
 #' Generate Data by Gaussian Graphical Model
-#' 
+#'
 #' @param p dimension of each pathway.
 #' @param K number of pathways.
 #' @param n number of observations.
 #' @param network_type choice of type of network: 'ar(2)', 'chain', 'random' or 'scale-free'.
-#' 
+#'
 #' @return a list of objets:
 #'  data data.
 #'  z_P pathway membership of genes.
-#'  A Precsion Matrix for the whole network. 
-#'  
+#'  A Precsion Matrix for the whole network.
+#'
 #' @export
 GenerateData <- function(p, K, n, network_type = "ar2") {
   # initialize
   z_P <- c()
   Omegas <- list()
   A <- matrix(0, p * K, p * K)
-  
+
   # set network for each block
   for (k in 1:K) {
     z_P <- c(z_P, rep(k, p))
-    if (network_type == "ar2") 
+    if (network_type == "ar2")
       Omega <- toeplitz(c(1, 0.5, 0.25, rep(0, p - 3)))
     if (network_type == "chain") {
       Omega <- make_ring(p)
       Omega <- as.matrix(get.adjacency(Omega))
       L <- Omega
       L[upper.tri(L)] <- 0
-      L <- ifelse(L == 1, sample(c(runif(1, -0.4, -0.1), runif(1, 
+      L <- ifelse(L == 1, sample(c(runif(1, -0.4, -0.1), runif(1,
                                                                0.1, 0.4)), 1), 0)
       Omega <- L + t(L) + diag(p)
     }
@@ -36,7 +36,7 @@ GenerateData <- function(p, K, n, network_type = "ar2") {
       Omega <- as.matrix(get.adjacency(Omega))
       L <- Omega
       L[upper.tri(L)] <- 0
-      L <- ifelse(L == 1, sample(c(runif(1, -0.4, -0.1), runif(1, 
+      L <- ifelse(L == 1, sample(c(runif(1, -0.4, -0.1), runif(1,
                                                                0.1, 0.4)), 1), 0)
       Omega <- L + t(L) + diag(p)
     }
@@ -51,15 +51,15 @@ GenerateData <- function(p, K, n, network_type = "ar2") {
     A[which(z_P == k), which(z_P == k)] <- Omega
     Omegas[[k]] <- Omega
   }
-  
+
   # make sure precision matrix must be positive definite
-  if (network_type == "scale-free") 
+  if (network_type == "scale-free")
     A <- FixMatrix(A, 1.5)
-  if (any(eigen(A)$values < 0)) 
+  if (any(eigen(A)$values < 0))
     stop("Precision matrix must be positive definite")
-  
+
   # generdate data based on precision matrix
   Cov_True <- solve(A)
-  data <- mvrnorm(n, rep(0, p * K), Cov_True)
+  data <- MASS::mvrnorm(n, rep(0, p * K), Cov_True)
   return(list(data = data, z_P = z_P, A = A))
 }
